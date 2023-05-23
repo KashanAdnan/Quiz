5a5414724d7d11080472645eafbd8caf736f5711
KashanAdnan <khanzaidaboy@gmail.com>
2023-05-23 23:29:36 +0500

    some-code

---
+++ app.js
@@ -0,0 +1,70 @@
+const container = document.querySelector(".container");
+const pwShowHide = document.querySelectorAll(".showHidePw");
+const pwFields = document.querySelectorAll(".password");
+const signUp = document.querySelector(".signup-link");
+const login = document.querySelector(".login-link");
+
+//   js code to show/hide password and change icon
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    pwFields.forEach((pwField) => {
+      if (pwField.type === "password") {
+        pwField.type = "text";
+
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("uil-eye-slash", "uil-eye");
+        });
+      } else {
+        pwField.type = "password";
+
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("uil-eye", "uil-eye-slash");
+        });
+      }
+    });
+  });
+});
+
+// js code to appear signup and login form
+signUp.addEventListener("click", () => {
+  container.classList.add("active");
+});
+login.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+function loginaccount() {
+  let email = document.getElementById("login-email").value;
+  let password = document.getElementById("login-password").value;
+  let users = JSON.parse(localStorage.getItem("users"));
+  var flage = false;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === email && users[i].password === password) {
+      flage = true;
+      alert("You  have successfully Login");
+      window.location.href = "./quiz.html";
+      localStorage.setItem("name", users[i].name);
+      localStorage.setItem("email", email);
+      localStorage.setItem("password", password);
+    }
+  }
+  if (flage == false) {
+    alert("Please enter a valid email address and  password");
+  }
+}
+
+function signUpA() {
+  let users = [];
+  let obj = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+  users = JSON.parse(localStorage.getItem("users"))
+    ? JSON.parse(localStorage.getItem("users"))
+    : [];
+  users.push(obj);
+
+  localStorage.setItem("users", JSON.stringify(users));
+  window.location.href = "./index.html";
+}
